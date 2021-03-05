eed047619ecebd1c9ca8f6047c484a87268974cc
James Fenn <me@jfenn.me>
2021-03-05 13:52:38 -0500

    add template variables

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+testuser = {
+	"id": "mobydick",
+	"name": "Moby Dick",
+	"image": "https://media.pri.org/s3fs-public/styles/story_main/public/images/2019/08/2031_episodeimage.jpg"
+}
 
 @app.route("/")
 def home():
     """Return the user's home page."""
     print("Hit the route!")
-    return render_template("index.html")
+    return render_template("index.html",
+		page = {},
+		user = testuser)
 
 @app.route("/login")
 def login():
@@ -19,24 +26,30 @@ def login():
 def user(user):
     """Return the user profile page."""
     return render_template("user.html",
-		user={"username": user, "displayName": "Moby Dick", "profileImage": "https://media.pri.org/s3fs-public/styles/story_main/public/images/2019/08/2031_episodeimage.jpg"})
+		page = { "user": { "id": user, "name": testuser["name"], "image": testuser["image"] }},
+		user = testuser)
 
 @app.route("/search")
 def search():
 	"""Return the results for a search query."""
 	return render_template("search.html",
-		query = request.args.get('q'))
+		page = { "query": request.args.get('q') },
+		user = testuser)
 
 @app.route("/friends")
 def friends():
 	"""Return the user's friends list."""
-	return render_template("friends.html")
+	return render_template("friends.html",
+		page = {},
+		user = testuser)
 
 @app.route("/notifications")
 def notifications():
 	"""Return the user's recent pokes."""
-	return render_template("notifications.html")
+	return render_template("notifications.html",
+		page = {},
+		user = testuser)
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True) 
+    app.run(host='127.0.0.1', port=8080, debug=True)
