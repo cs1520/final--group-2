a8bbddcf5f2b18f9900a6fd2be357aed6140beae
James Fenn <me@jfenn.me>
2021-04-07 12:32:22 -0400

    add prefix to autocomplete keys (avoid conflicting with dark mode setting)

--- static/js/utils.js
+++ static/js/utils.js
@@ -22,3 +22,33 @@ export function debounce(callback, delay) {
 		}, delay);
 	};
 }
+
+/**
+ * Obtains an object/dict of all storage values with the given
+ * prefix value.
+ *
+ * e.g. if the prefix is "data-" and localStorage contains
+ *   { "data-hello": "world", "data-apple": "monch", "unrelated-value": "hi" }
+ * then the returned value will be
+ *   { "hello": "world", "apple": "monch" }
+ *
+ * @param prefix - The prefix to search for
+ * @returns A dict of all keys matching the prefix
+ */
+export function allStorage(prefix) {
+	let values = {};
+
+	// get all keys from localStorage
+	let keys = Object.keys(localStorage);
+	for (let key of keys) {
+		// if key doesn't have prefix, ignore
+		if (!key.startsWith(prefix))
+			continue;
+
+		// remove prefix from key & insert in values
+		let valueKey = key.substr(prefix.length);
+		values[valueKey] = localStorage.getItem(key);
+	}
+
+	return values;
+}
